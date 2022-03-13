182c883a78de18e0fd00415b73ef22c5c1d29435
Tiancheng Jiao <43341500+jtc1246@users.noreply.github.com>
2022-03-13 16:21:38 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cryptoEx',
-    version='0.0.2',
+    version='0.0.3',
     author='Tiancheng Jiao',
     author_email='jtc1246@outlook.com',
     url='https://github.com/jtc1246/cryptoEx',
@@ -11,21 +11,5 @@ setup(
     install_requires=['urllib3'],
     python_requires='>=3',
     platforms=["all"],
-    license='GPL-2.0 License',
-    entry_points={
-        'console_scripts': [
-            'bnGet=cryptoEx:bnGet',
-            'bnPPD=cryptoEx:bnPPD',
-            'bnSign=cryptoEx:bnSign',
-            'hbGet=cryptoEx:hbGet',
-            'hbPost=cryptoEx:hbPost',
-            'hbSign=cryptoEx:hbSign',
-            'okGet=cryptoEx:okGet',
-            'okPost=cryptoEx:okPost',
-            'okSign=cryptoEx:okSign',
-            'kkGet=cryptoEx:kkGet',
-            'kkPost=cryptoEx:kkPost',
-            'kkSign=cryptoEx:kkSign'
-        ]
-    }
+    license='GPL-2.0 License'
 )
